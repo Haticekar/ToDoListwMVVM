81c16ebafba974697b53874b11cd28e5a47a7014
Haticekar <hatticekar14@gmail.com>
2024-11-09 17:02:06 +0300

    Initial Commit

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Hatice Kar on 9.11.2024.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
