f7804f1c6c777d08896c405000741f4834f21687
Haticekar <hatticekar14@gmail.com>
2024-11-10 16:23:05 +0300

    commit

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+/*
+ MVVM Architecture
+ 
+ Model - data point
+ View - UI
+ ViewModel - manages Models for View
+ 
+ 
+ */
 
 @main
 struct ToDoListApp: App {
+    
+    @State var listViewModel : ListViewModel = ListViewModel()
+    
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                ListView()
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .environmentObject(listViewModel)
+           
         }
     }
 }
--- ToDoList/View/ListView.swift
+++ ToDoList/View/ListView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct ListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    @EnvironmentObject var listViewModel : ListViewModel
+    
+    /*@State var items : [ItemModel] = [
+        ItemModel(title: "This is first title!", isCompleted: false),
+        ItemModel(title: "This is the second!", isCompleted: true),
+        ItemModel(title: "Third!", isCompleted: false)
+    ]*/
+    
+    var body : some View {
+        
+        ZStack{
+            
+            if listViewModel.items.isEmpty{
+                NoItemsView()
+                    //.transaction //(AnyTransition.opacity.animation(.easeIn))
+            }else{
+                List {
+                    ForEach(listViewModel.items) {
+                        item in ListRowView(item: item, title: "")
+                            .onTapGesture {
+                                withAnimation(.linear){
+                                    listViewModel.updateItem(item: item)
+                                    
+                                }
+                            }
+                    }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(PlainListStyle())
+            }
+            
+        }
+      
+        .navigationTitle("ToDo List 📋")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing: NavigationLink("Add", destination: AddView()))
+                   
     }
+   
 }
-
 #Preview {
-    ListView()
+    NavigationView{
+        ListView()
+    }
+    .environmentObject(ListViewModel())
 }
